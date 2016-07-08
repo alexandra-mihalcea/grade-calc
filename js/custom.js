7846c29bcfc19af45f7d8cddd4be0332b809e1d0
alexandra-mihalcea <alexandramihalcea15@gmail.com>
2016-07-08 11:38:20 +0100

    Added functionality to calculate needed grade

--- js/custom.js
+++ js/custom.js
@@ -24,7 +24,6 @@ $(function()
 
 function calculateAverage() {
 	var formData = document.getElementById("formdata");
-	var text ="hey";
 	marks=[];
 	weight=[];
 	var sum = 0, counter=0, temp=0, count1=0, count2=0;
@@ -58,3 +57,62 @@ function calculateAverage() {
 	document.getElementById("result").innerHTML =avg;
 }
 
+function findNeeded() {
+	var formData = document.getElementById("formdata");
+	var formData2 = document.getElementById("formdesired");
+	var desiredGrade=formData2.elements[0].value;
+	var remainingWeight=formData2.elements[1].value;
+	marks=[];
+	weight=[];
+	var sum = 0, counter=0, temp=0, count1=0, count2=0, x=0;
+	
+	for( var i = 0; i < formData.length; i++ ){
+		if(formData.elements[i].value!= 0){
+			if(i%3==0){
+				marks[count1]=formData.elements[i].value;
+				count1++;}
+			else if(i%3==1){
+				weight[count2]=formData.elements[i].value;
+				count2++;}
+		}
+	}
+	
+	for( var i = 0; i < marks.length; i++ ){
+		if (weight.length==marks.length){
+			temp=weight[i]/10;}
+		if(temp==0){
+			temp=1;	
+		}
+		sum += marks[i]*temp;
+		counter+=temp;	
+	}
+	
+	
+	if(desiredGrade!=0 && remainingWeight!=0){
+		remainingWeight=remainingWeight/10;}
+	else{
+		remainingWeight=1;	
+		}
+	counter+=remainingWeight;
+	if(desiredGrade!==0){
+		x=((desiredGrade*counter) - sum)/remainingWeight;
+	}
+	else{
+		x=0;	
+	}
+	document.getElementById("requiredMark").innerHTML = x;
+	var count=0;
+	while(desiredGrade>temp){
+		temp=Math.pow(10, count);
+		count++;
+	}
+	if(0<x&&x<temp){
+		document.getElementById("greeting").innerHTML = "Good luck!";
+	}
+	else if(x==0){
+		document.getElementById("greeting").innerHTML = " ";
+		}
+	else{
+		document.getElementById("greeting").innerHTML = "Sorry!";
+		}
+}
